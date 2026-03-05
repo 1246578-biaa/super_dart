4be6dd15554d01025cfb6083c9716ff80d7459b8
Wahyu Oktavia <wahyuoktavia1176@gmail.com>
2026-03-05 10:38:36 +0700

    menambahkan kalkulator.dart

---
+++ kalkulator.dart
@@ -0,0 +1,13 @@
+void main() {
+
+  int a = 10;
+  int b = 5;
+  int hasil = a + b;
+  print('Hasil $a + $b = $hasil'); 
+
+  print('Hasil fungsi: ${hitungTambah(10, 20)}');
+}
+
+int hitungTambah(int x, int y) {
+  return x + y;
+}
